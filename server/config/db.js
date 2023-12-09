0dd4b6ff2b4d8779cbe98f45dd6a4c46851c2147
Paweł Piotrowski <pawel.piotrowski38@gmail.com>
2023-12-09 16:31:52 +0100

    Create env database connection variables

--- server/config/db.js
+++ server/config/db.js
@@ -1,12 +1,15 @@
 import pkg from 'pg';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const { Pool } = pkg;
 
 const pool = new Pool({
-    host: 'localhost',
-    user: 'postgres',
-    password: 'Qwerty123',
-    database: 'moviesdatabase',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
 });
 
 pool.on('acquire', (connection) => {
