eaa9dc9f6e4539abc2902b24884b0ac4205f34b5
Paweł Piotrowski <pawel.piotrowski38@gmail.com>
2023-12-10 18:43:21 +0100

    Create constants file

---
+++ client/src/data/constants.js
@@ -0,0 +1,28 @@
+export const genresValues = [
+    'drama', 'comedy', 'thriller', 'action', 'romance', 'horror', 'adventure', 'crime',
+    'sci-fi', 'fantasy', 'mystery', 'animation', 'biography', 'music', 'family', 'history',
+    'war', 'musical', 'sport', 'documentary', 'film-noir', 'western'
+];
+
+export const countriesValues = [
+    'usa', 'india', 'china', 'uk', 'france', 'japan', 'south-korea', 'canada', 'germany',
+    'australia', 'spain', 'italy', 'russia', 'mexico', 'brazil', 'hong-kong', 'nigeria',
+    'sweden', 'norway', 'netherlands', 'denmark', 'iran', 'taiwan', 'argentina', 'turkey',
+    'indonesia', 'poland', 'thailand', 'south-africa', 'egypt', 'philippines', 'finland',
+    'greece', 'austria', 'belgium', 'czech-republic', 'switzerland', 'ireland', 'israel',
+    'hungary', 'portugal', 'romania', 'new-zealand'
+];
+
+export const yearsValues = [
+    '2023', '2022', '2021', '2020', '2019', '2018', '2017', '2016', '2015', '2014', '2013', '2012',
+    '2011', '2010', '2009', '2008', '2007', '2006', '2005', '2004', '2003', '2002', '2001', '2000',
+    '1999', '1998', '1997', '1996', '1995', '1994', '1993', '1992', '1991', '1990'
+];
+
+export const moviesSortValues = [
+    'title_asc', 'title_desc', 'rating_desc', 'rating_asc', 'year_desc', 'year_asc'
+];
+
+export const moviesSortTexts = [
+    'Title (A-Z)', 'Title (Z-A)', 'Highest rating', 'Lowest rating', 'Newest', 'Oldest'
+];
