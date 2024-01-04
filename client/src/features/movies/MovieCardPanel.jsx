a500341b09cc63aa3211968a76bf342be4edc0ac
Paweł Piotrowski <pawel.piotrowski38@gmail.com>
2024-01-04 19:50:45 +0100

    Modify rating panel component

--- client/src/features/movies/MovieCardPanel.jsx
+++ client/src/features/movies/MovieCardPanel.jsx
@@ -1,19 +1,8 @@
-import { useState } from 'react';
-import Button from '../../ui/Button';
 import CloseButton from '../../ui/CloseButton';
-import Heading from '../../ui/Heading';
 import RatingPanel from '../../ui/RatingPanel';
 import './movieCardPanel.scss';
-import RatingPanelButton from '../../ui/RatingPanelButton';
 
 export default function MovieCardPanel({ onSetIsPanelVisible }) {
-    const [rating, setRating] = useState(0);
-    const [tempRating, setTempRating] = useState(0);
-
-    const handleRating = function(rating) {
-        setRating(rating);
-    }
-
     return (
         <div className='movie-card-panel'>
             <div className='movie-card-panel__close-button-container'>
@@ -21,37 +10,7 @@ export default function MovieCardPanel({ onSetIsPanelVisible }) {
                     onCloseHandler={() => onSetIsPanelVisible(false)}
                 />
             </div>
-            <div className='movie-card-panel__ratings-container'>
-                <Heading
-                    type={'h2'}
-                    size={'1rem'}
-                    weight={500}
-                >
-                    {rating || tempRating ? (
-                        `Your rating: ${tempRating ? tempRating : rating}`
-                    ) : (
-                        'Rate movie'
-                    )}
-                </Heading>
-                <RatingPanel>
-                    {Array.from({ length: 10 }, (_, i) => (
-                        <RatingPanelButton
-                            key={i}
-                            size={'32px'}
-                            full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
-                            onHandleRating={() => handleRating(i + 1)}
-                            onHandleHoverIn={() => setTempRating(i + 1)}
-                            onHandleHoverOut={() => setTempRating(0)}
-                        />
-                    ))}
-                </RatingPanel>
-                <Button
-                    width={'100%'}
-                    fontSize={'0.875rem'}
-                >
-                    Add to watchlist
-                </Button>
-            </div>
+            <RatingPanel />
         </div>
     )
 }
--- client/src/ui/RatingPanel.jsx
+++ client/src/ui/RatingPanel.jsx
@@ -1,10 +1,53 @@
+import { useState } from 'react';
+import Heading from './Heading';
+import RatingPanelButton from './RatingPanelButton';
+import Button from './Button';
 import './ratingPanel.scss';
 
-export default function RatingPanel({ children }) {
+export default function RatingPanel() {
+    const [rating, setRating] = useState(0);
+    const [tempRating, setTempRating] = useState(0);
+
+    const handleRating = function(rating) {
+        setRating(rating);
+    }
+
     return (
         <div className='rating-panel'>
             <div className='rating-panel__ratings-container'>
-                {children}
+                <div className='rating-panel__user-rating-container'>
+                    <Heading
+                        type={'h2'}
+                        size={'1rem'}
+                        weight={500}
+                    >
+                        {rating || tempRating ? (
+                            `Your rating: ${tempRating ? tempRating : rating}`
+                        ) : (
+                            'Rate movie'
+                        )}
+                    </Heading>
+                </div>
+                <div className='rating-panel__ratings'>
+                    {Array.from({ length: 10 }, (_, i) => (
+                        <RatingPanelButton
+                            key={i}
+                            size={'32px'}
+                            full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
+                            onHandleRating={() => handleRating(i + 1)}
+                            onHandleHoverIn={() => setTempRating(i + 1)}
+                            onHandleHoverOut={() => setTempRating(0)}
+                        />
+                    ))}
+                </div>
+            </div>
+            <div className='rating-panel__buttons-container'>
+                <Button
+                    width={'100%'}
+                    fontSize={'0.875rem'}
+                >
+                    Add to watchlist
+                </Button>
             </div>
         </div>
     )
