3613e7a75ae84e6e7f54289a4c6f8beabb06822f
Paweł Piotrowski <pawel.piotrowski38@gmail.com>
2024-02-10 15:11:48 +0100

    Create and update authentication endpoints

--- server/routes/authentication.js
+++ server/routes/authentication.js
@@ -1,6 +1,10 @@
 import { Router } from 'express';
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 import pool from '../config/db.js';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const router = Router();
 
@@ -26,18 +30,22 @@ router.get('/check-user', async (req, res) => {
         connection = await pool.connect();
 
         try {
-            const usernameQuery = 'SELECT username, theme FROM users WHERE user_id = $1'
-            const [usernameResults] = await connection.query(usernameQuery, [userId]);
+            const selectUserQuery = `
+                SELECT username, theme
+                FROM users
+                WHERE user_id = $1;
+            `;
 
-            if (usernameResults.length === 0) {
-                connection.release();
-                return res.sendStatus(404);
+            const selectUserResults = await connection.query(selectUserQuery, [userId]);
+
+            if (selectUserResults.rowCount === 0) {
+                return res.status(200).json({ message: 'User not found' });
             }
 
-            const username = usernameResults[0].username;
-            const theme = usernameResults[0].theme;
+            const username = selectUserResults.rows[0].username;
+            const theme = selectUserResults.rows[0].theme;
 
-            res.status(200).json({ id: userId, username: username, theme: theme });
+            res.status(200).json({ userId: userId, username: username, theme: theme });
         } catch (error) {
             console.log('Error:', error);
             res.status(500).json({ message: 'Internal server error', err: error });
@@ -56,10 +64,8 @@ router.get('/check-user', async (req, res) => {
     }
 });
 
-router.post('/login', async (req, res) => {
-    const { username, password } = req.body;
-
-    console.log('asd');
+router.post('/signup', async (req, res) => {
+    const { emailAddress, username, password } = req.body;
 
     const accessToken = req.cookies?.accessToken;
     const refreshToken = req.cookies?.refreshToken;
@@ -67,8 +73,7 @@ router.post('/login', async (req, res) => {
     if (accessToken || refreshToken) {
         try {
             const decodedToken = jwt.verify(refreshToken, process.env.TOKEN_KEY);
-            console.log('Decoded token: ', decodedToken);
-            return res.send('User is logged in');
+            return res.status(409).json({ message: 'User is logged in' });
         } catch (error) {
             return res.status(401).json({ message: 'Invalid token' });
         }
@@ -79,37 +84,91 @@ router.post('/login', async (req, res) => {
     try {
         connection = await pool.connect();
 
-        const userResults = await connection.query('SELECT user_id FROM users WHERE username = $1', [username]);
+        const checkEmailQuery = `
+            SELECT user_id FROM users
+            WHERE email = $1;
+        `;
+
+        const checkEmailResults = await connection.query(checkEmailQuery, [emailAddress]);
         
-        if (userResults.rowCount === 0) {
-            res.status(201).json({ message: 'User not found' });
+        if (checkEmailResults.rowCount) {
+            res.status(409).json({ message: 'Email address already in use' });
             return;
         }
 
-        const userId = userResults.rows[0].user_id;
+        const checkUsernameQuery = `
+            SELECT user_id FROM users
+            WHERE username = $1;
+        `;
 
-        const verifiedResults = await connection.query('SELECT is_verified FROM users WHERE user_id = $1', [userId]);
+        const checkUsernameResults = await connection.query(checkUsernameQuery, [username]);
+            
+        if (checkUsernameResults.rowCount) {
+            res.status(409).json({ message: 'Username already in use' });
+            return;
+        }
 
-        if (!verifiedResults.rows[0].is_verified) {
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const insertUserQuery = `
+            INSERT INTO users (email, username, password, is_verified)
+            VALUES ($1, $2, $3, $4);
+        `
+
+        const insertUserResults = await connection.query(insertUserQuery, [emailAddress, username, hashedPassword, true]);
+
+        res.status(201).json({ message: 'User has been created successfully' });
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ message: 'Internal server error', err: error });
+    } finally {
+        if (connection) {
+            connection.release();
+        }
+    }
+});
+
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    const accessToken = req.cookies?.accessToken;
+    const refreshToken = req.cookies?.refreshToken;
+
+    if (accessToken || refreshToken) {
+        try {
+            const decodedToken = jwt.verify(refreshToken, process.env.TOKEN_KEY);
+            return res.status(409).json({ message: 'User is logged in' });
+        } catch (error) {
+            return res.status(401).json({ message: 'Invalid token' });
+        }
+    }
+
+    let connection = null;
+
+    try {
+        connection = await pool.connect();
+
+        const selectUserQuery = `
+            SELECT user_id, password, is_verified, theme
+            FROM users
+            WHERE username = $1;
+        `
+
+        const selectUserResults = await connection.query(selectUserQuery, [username]);
+        
+        if (selectUserResults.rowCount === 0) {
+            res.status(401).json({ message: 'User not found' });
+            return;
+        }
+
+        if (!selectUserResults.rows[0].is_verified) {
             res.status(401).json({ message: 'Account is not verified' });
             return;
         }
 
-        const accountQuery = `
-            SELECT u.hashed_password, u.is_verified, u.dfa_enabled, u.user_role,
-            t.theme, t.schedule_enabled, t.start_hour, t.start_minute, t.end_hour, t.end_minute, t.language,
-            p.rank_name
-            FROM heroku_4ca5abe69b33068.users u
-            INNER JOIN heroku_4ca5abe69b33068.users_points p ON u.id = p.user_id
-            INNER JOIN heroku_4ca5abe69b33068.users_preferences t ON u.id = t.user_id
-            WHERE u.id = ?
-        `
+        const userId = selectUserResults.rows[0].user_id;
 
-        const [accountResults] = await connection.query(accountQuery, [userId]);
-
-        const theme = accountResults[0].theme;
-
-        const isMatch = await bcrypt.compare(password, accountResults[0].hashed_password);
+        const isMatch = await bcrypt.compare(password, selectUserResults.rows[0].password);
 
         if (!isMatch) {
             res.status(401).json({ message: 'Invalid password' });
@@ -121,13 +180,13 @@ router.post('/login', async (req, res) => {
         const hashedRefreshToken = await bcrypt.hash(refreshToken, 10);
         const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000); // 7 days
 
-        res.cookie('accessToken', accessToken, { httpOnly: true, secure: true, domain: '.moviesworld.one', sameSite: 'none', maxAge: 10 * 60 * 1000 });
-        res.cookie('refreshToken', refreshToken, { httpOnly: true, secure: true, domain: '.moviesworld.one', sameSite: 'none', maxAge: 7 * 24 * 60 * 60 * 1000 });
+        res.cookie('accessToken', accessToken, { httpOnly: true, secure: true, sameSite: 'none', maxAge: 10 * 60 * 1000 });
+        res.cookie('refreshToken', refreshToken, { httpOnly: true, secure: true, sameSite: 'none', maxAge: 7 * 24 * 60 * 60 * 1000 });
 
-        const [deleteResults] = await connection.query('DELETE FROM refresh_tokens WHERE user_id = ?', [userId]);
-        const [insertResults] = await connection.query('INSERT INTO refresh_tokens (user_id, token, expires_at) VALUES (?, ?, ?)', [userId, hashedRefreshToken, expiresAt]);
+        const deleteTokenResults = await connection.query('DELETE FROM refresh_tokens WHERE user_id = $1', [userId]);
+        const insertTokenResults = await connection.query('INSERT INTO refresh_tokens (user_id, token, expires_at) VALUES ($1, $2, $3)', [userId, hashedRefreshToken, expiresAt]);
 
-        res.status(201).json({ theme: theme, message: 'Login successful' });
+        res.status(201).json({ theme: selectUserResults.rows[0].theme, message: 'Login successful' });
     } catch (error) {
         console.error('Error:', error);
         res.status(500).json({ message: 'Internal server error', err: error });
@@ -136,6 +195,37 @@ router.post('/login', async (req, res) => {
             connection.release();
         }
     }
+});
+
+router.delete('/logout', async (req, res) => {
+    const refreshToken = req.cookies?.refreshToken;
+
+    let connection = null;
+
+    try {
+        const decodedRefreshToken = jwt.verify(refreshToken, process.env.TOKEN_KEY);
+        const userId = decodedRefreshToken.userId;
+
+        connection = await pool.connect();
+
+        const deleteTokenQuery = `
+            DELETE FROM refresh_tokens
+            WHERE user_id = $1;
+        `;
+
+        const deleteTokenResults = await connection.query(deleteTokenQuery, [userId]);
+        console.log('Refresh token has been deleted from database');
+    } catch (error) {
+        console.log('Refresh token is invalid or expired');
+    } finally {
+        if (connection) {
+            connection.release();
+        }
+    }
+
+    res.clearCookie('accessToken');
+    res.clearCookie('refreshToken');
+    res.status(200).json({ message: 'Cookies have been removed' });
 });
 
 export default router;
